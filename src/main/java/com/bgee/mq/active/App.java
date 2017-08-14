a888ce8ae0f38ab0593badd8939a2a706a5c35e2
bgee0915 <bgee0915@outlook.com>
2017-08-14 21:44:02 +0800

    first

---
+++ src/main/java/com/bgee/mq/active/App.java
@@ -0,0 +1,13 @@
+package com.bgee.mq.active;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
